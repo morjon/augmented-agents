38f386305cadeed0afb013af79257f47d54eb32f
Ubuntu <ubuntu@ip-172-31-6-140.us-west-2.compute.internal>
2023-05-31 06:39:53 +0000

    tweak : llm class

--- chain_sim/local_model.py
+++ chain_sim/local_model.py
@@ -1,9 +1,10 @@
 from typing import Any, List, Optional
-from langchain.llms.base import LlamaCpp
+from langchain.llms import LlamaCpp
 
 
 class local_llama(LlamaCpp):
-    def __init__(self, model_path: str, **kwargs: Any):
+    def __init__(self, **kwargs: Any):
+        model_path = "../llama.cpp/models/13B/ggml-model-q4_0.bin"
         super().__init__(model_path=model_path, **kwargs)
 
     def generate_agent_text(
@@ -21,3 +22,11 @@ class local_llama(LlamaCpp):
             top_p=top_p,
             stop=stop,
         )
+
+
+if __name__ == "__main__":
+    llama = local_llama()
+    prompt = "Hello, how are you today?"
+    response = llama.generate_agent_text(prompt)
+    print(f"Prompt: {prompt}")
+    print(f"Response: {response}")
